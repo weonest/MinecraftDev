c52700611053d2575dabe6234e648e0d8e8dff33
weonest <geonhee33@naver.com>
2023-05-13 21:59:07 +0900

    Command 작성 및 입장 퇴장 메세지

---
+++ Event2/src/main/java/me/weonest/event/JoinLeaveListener.java
@@ -0,0 +1,31 @@
+package me.weonest.event;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JoinLeaveListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (!player.hasPlayedBefore()) {
+            event.setJoinMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + player.getDisplayName() + ChatColor.GREEN + " 님이 이세계에 도착했습니다");
+        }else {
+            event.setJoinMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + player.getDisplayName() + ChatColor.GREEN + " 새로운 게이가 이세계에 소환됐습니다");
+        }
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        event.setQuitMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + player.getDisplayName() + ChatColor.RED + " 님이 현생으로 떠났습니다");
+    }
+
+
+}
--- MyFirstPlugin/src/main/java/me/weonest/myfirstplugin/MyFirstPlugin.java
+++ MyFirstPlugin/src/main/java/me/weonest/myfirstplugin/MyFirstPlugin.java
@@ -1,17 +1,22 @@
 package me.weonest.myfirstplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
+public final class MyFirstPlugin extends JavaPlugin{
 
-public final class MyFirstPlugin extends JavaPlugin {
-
-    @Override
+    @Override // 플러그인 시작 시
     public void onEnable() {
         // Plugin startup logic
 
+        System.out.println("### 플러그인이 시작되었습니다 ###");
+
     }
 
-    @Override
+    @Override // 플러그인 종료 시
     public void onDisable() {
         // Plugin shutdown logic
+
+
+        System.out.println("### 플러그인이 종료되었습니다 ###");
     }
 }
